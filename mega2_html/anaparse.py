8c4fce19d4ac8533fbbb3e5b84a0f28b759903fb
R. V. Baron <rvb5@pitt.edu>
2014-05-22 10:51:23 -0400

    make anaparse.py oo. add second level template for use with anchors

--- mega2_html/anaparse.py
+++ mega2_html/anaparse.py
@@ -8,9 +8,9 @@ import pdb
 
 ANALYSIS = "wik"
 TEMPLATE = "template.html"
-#URME     = "file:///Users/rbaron/mega2/bb/mega2_html/mega2_documentation.html"
-URME     = "http://watson.hgen.pitt.edu/docs/mega2_html/mega2_documentation.html"
-
+TEMPLAT2 = "template_mega2.html"
+#URME     = "file:///Users/rbaron/mega2/bb/mega2_html"
+URME     = "http://watson.hgen.pitt.edu/docs/mega2_html"
 
 HEAD = re.compile("""\|\s*(.*?)<ref""")
 HED  = re.compile("""\|\s*(.*?)\s*\|\|""")
@@ -97,36 +97,93 @@ def show_input():
 #               print('0,', end="")
 #       print(']\n')
 
-elements = (('title','{0}. '), ('author','{0}. '), \
-            ('last','{0} '),   ('first','{0}, '), ('coauthors','{0}. '), \
-            ('date','({0}) '), ('journal','{0} '), \
-            ('volume','{0}'),  ('issue','({0})'), \
-            ('pages',':{0}.'), ('url','<span><a href="{0}">{0}</a></span>'))
-def mkcit(href, citname):
-    cit = DBREF[citname]
-    text = []
-    for el in elements:
-        if el[0] in cit:
-            text.append(el[1].format(cit[el[0]]))
-    href.append("".join(text))
+class Citation(object):
+    match = None
+    repl  = None
+    all   = []
 
-def main():
-    MT = []
+    def __init__(self, m, r):
+        self.match = m
+        self.repl  = r
+        self.all   = []
 
-    parse(ANALYSIS)
+    def __iter__(self):
+        return self.all.__iter__()
 
-    def prune(el):
-        return el.endswith("format")
-    analysis = filter(prune, sorted(DB.iterkeys()))
-    
-    if not os.path.isfile(TEMPLATE):
-        print("Not a File: {0}".format(TEMPLATE))
-    File = open(TEMPLATE)
-    Lines = File.readlines()
-    File.close()
+    def mega2(self, anchr, urme, tag, anly):
+        self.all.append(anchr.format(urme, tag.replace(self.match, self.repl), anly) )
 
-    TabFile = open('table.html', "w")
-    print("""<!--
+    def url(self, ustr):
+        self.all.append('  <a href="{0[0]}">{0[1]}</a>  '.format(ustr) )
+
+    elements = (('title','{0}. '), ('author','{0}. '), \
+                ('last','{0} '),   ('first','{0}, '), ('coauthors','{0}. '), \
+                ('date','({0}) '), ('journal','{0} '), \
+                ('volume','{0}'),  ('issue','({0})'), \
+                ('pages',':{0}.'), ('url','<span><a href="{0}">{0}</a></span>'))
+    def mkcite(self, citename):
+        cite = DBREF[citename]
+        text = []
+        for el in self.elements:
+            if el[0] in cite:
+                text.append(el[1].format(cite[el[0]]))
+        self.all.append("".join(text))
+
+class Template(object):
+    repl = {}
+    href = {}
+    lines= []
+    MT   = []
+
+    def __init__(self, Filename):
+        if not os.path.isfile(Filename):
+            print("Not a File: {0}".format(Filename))
+        File = open(Filename)
+        self.lines = File.readlines()
+        File.close()
+
+    def map(self, m, r):
+        self.repl[m] = r
+
+    def mapcit(self, m, r):
+        self.repl[m] = '<ul >'
+        self.href[m] = r
+
+    def write(self, Filename):
+        File = open(Filename, "w")
+
+        ks = self.repl.keys()
+
+        for line in self.lines:
+            newline = line
+            href = self.MT
+
+            for k in ks:
+                if k in newline:
+                    newline = newline.replace(k, self.repl[k])
+                    href = self.href.get(k, href)
+
+            print(newline, file=File, end="")
+
+            for xline in href:
+                print('<li>{0}</li>'.format(xline), file=File)
+            if href:
+                print('</ul>', file=File)
+        File.close()
+
+
+class Table(object):
+    File   = None
+    TabCol = 0
+    TabRow = 0
+
+    def __init__(self, Filename):
+        self.File = open(Filename, "w")
+        self.TabCol = 1
+        self.TabRow = 1
+
+    def css(self):
+        print("""<!--
   <link rel="stylesheet" href="Mega2_Conversions.css" type="text/css" media="all"/>
 -->
 <style type="text/css">
@@ -160,25 +217,80 @@ tr.RowOdd {
 tr.RowEven {
 }
 </style>
-""", file=TabFile)
-    print('<table>', file=TabFile)
-    print('<tr class="RowOdd">', file=TabFile)
-    print('<th class="ColOdd">LINKAGE</th><th class="ColEven">Mega2</th>', file=TabFile)
-    print('<th class="ColOdd">PLINK</th><th class="ColEven">VCF or BCF</th>', file=TabFile)
-    print('</tr>', file=TabFile)
-    TabCol = 1
-    TabRow = 2
+""", file=self.File)
+
+    def head(self):
+        print('<table>', file=self.File)
+        print('<tr class="RowOdd">', file=self.File)
+        print('<th class="ColOdd">LINKAGE</th><th class="ColEven">Mega2</th>', file=self.File)
+        print('<th class="ColOdd">PLINK</th><th class="ColEven">VCF or BCF</th>', file=self.File)
+        print('</tr>', file=self.File)
+        self.TabRow = self.TabRow + 1
+
+    def row(self):
+        print('<tr class="{0}">'.format("RowOdd" if (self.TabRow & 1) else "RowEven"), file=self.File)
+
+    def endrow(self):
+        print('</tr>', file=self.File)
+        self.TabRow = self.TabRow + 1
+
+    def col(self, Filename, analy):
+        print('<td class="{0}">'.format("ColOdd" if (self.TabCol & 1) else "ColEven"), file=self.File)
+        print('<a href="{0}">to {1}'.format(Filename, analy), file=self.File)
+
+    def endcol(self):
+        print('</td>', file=self.File)
+        self.TabCol = self.TabCol + 1
+
+    def close(self):
+        print('</table>', file=self.File)
+        self.File.close()
+
+
+def main():
+    MT = []
+
+    parse(ANALYSIS)
+    analysis = [ k for k in sorted(DB.iterkeys()) if k.endswith("format") ]
+
+    T  = Template(TEMPLATE)
+    T2 = Template(TEMPLAT2)
+
+    TabFile = Table('table.html')
+    TabFile.css()
+    TabFile.head()
+
 #   show_input()
+    for inp in ('LINKAGE', 'Mega2', 'PLINK', 'VCF or BCF'):
+        inp = inp.replace(' ', '_').lower()
+        T2.map('**path**', URME)
+        T2.map('**anchor**', "#inp:{0}".format(inp))
+        file_name = "conversions/frame_inp_{0}.html".format(inp)
+        T2.write(file_name)
 
     for analy in analysis:
-        an = analy.split()[0]
+        an  = analy.split()[0]
+        anr = an.replace('/', '').lower()
 
-        ahref = []
-        ahref.append( '<a href="{0}#ext:{1}"> Mega2 Analysis documentation: {2}</a>'.format(URME, an.lower().replace('/', ''), analy) )
+        T2.map('**path**', URME)
+        T2.map('**anchor**', "#ext:{0}".format(anr))
+        file_name = "conversions/frame_ext_{0}.html".format(anr)
+        T2.write(file_name)
+
+        ahref = Citation('/', '')
+
+#       ahref.mega2('<a href="{0}#ext:{1}"> Mega2 Analysis documentation: {2}</a>',
+#                   URME, an.lower(), analy)
+        file_name = "frame_ext_{0}.html".format(anr)
+        ahref.mega2('<a href="{0}"> Mega2 Analysis documentation: {2}</a>',
+                    file_name, anr, analy)
+
         for url in DB[analy][1]:
-            ahref.append('  <a href="{0[0]}">{0[1]}</a>  '.format(url) )
-        for citname in DB[analy][0]:
-            mkcit(ahref, citname)
+            ahref.url(url)
+
+        for citename in DB[analy][0]:
+            ahref.mkcite(citename)
+
         print('\n{0}, "{1}"\n\t{2}\n\t{3}\n'.format(an, analy, DB[analy][1], DB[analy][0]))
 
 #       for ref in DB[analy][0]:
@@ -188,56 +300,38 @@ tr.RowEven {
 #               print('0,', end="")
 #       print(']\n')
 
-        print('<tr class="{0}">'.format("RowOdd" if (TabRow & 1) else "RowEven"), file=TabFile)
-        TabRow = TabRow + 1
+        TabFile.row()
+
         for inp in ('LINKAGE', 'Mega2', 'PLINK', 'VCF or BCF'):
-            ihref = []
-            ihref.append( '<a href="{0}#inp:{1}"> Mega2 Input documentation: {2}</a>'.format(URME, inp.lower().replace(' ', '_'), inp) )
+            inpr = inp.replace(' ', '_').lower()
+
+            ihref = Citation(' ', '_')
+#           ihref.mega2('<a href="{0}#inp:{1}"> Mega2 Input documentation: {2}</a>',
+#                       URME, inp.lower(), inp)
+            file_name = "frame_inp_{0}.html".format(inpr)
+            ihref.mega2('<a href="{0}"> Mega2 Input documentation: {2}</a>',
+                        file_name, inpr, inp)
+
             for url in DB[inp][1]:
-                ihref.append('  <a href="{0[0]}">{0[1]}</a>  '.format(url) )
-            for citname in DB[inp][0]:
-                mkcit(ihref, citname)
+                ihref.url(url)
 
-            file_name = "conversions/{0}_{1}.html".format(inp.replace(' ', '_'), an.replace('/',''))
-            file_name = file_name.lower()
+            for citename in DB[inp][0]:
+                ihref.mkcite(citename)
 
-            print('<td class="{0}">'.format("ColOdd" if (TabCol & 1) else "ColEven"), file=TabFile)
-            TabCol = TabCol + 1
-            print('<a href="{0}">to {1}'.format(file_name, analy), file=TabFile)
+            file_name = "conversions/{0}_{1}.html".format(inpr, anr)
 
-            File = open(file_name, "w")
-            
-            for line in Lines:
-                newline = line
-                href = MT
+            TabFile.col(file_name, analy)
 
-                if newline.find('**input**') > -1:
-                    newline = newline.replace('**input**', inp)
+            T.map('**input**', inp)
+            T.map('**analysis**', analy)
+            T.mapcit('**ihref**', ihref)
+            T.mapcit('**ahref**', ahref)
+            T.write(file_name)
 
-                if newline.find('**analysis**') > -1:
-                    newline = newline.replace('**analysis**', analy)
+            TabFile.endcol()
 
-                if newline.find('**ihref**') > -1:
-                    newline = newline.replace('**ihref**', '<ul >')
-                    href = ihref
+        TabFile.endrow()
 
-                if newline.find('**ahref**') > -1:
-                    newline = newline.replace('**ahref**', '<ul >')
-                    href = ahref
-
-                print(newline, file=File, end="")
-
-                for xline in href:
-                    print('<li>{0}</li>'.format(xline), file=File)
-                if href:
-                    print('</ul>', file=File)
-
-            print('</td>', file=TabFile)
-            File.close()
-
-        print('</tr>', file=TabFile)
-
-    print('</table>', file=TabFile)
     TabFile.close()
 
 
