b9ddbc9a813ec8eab1d5b6f7b37d5c34d78635bd
R. V. Baron <rvb5@pitt.edu>
2015-06-18 19:53:00 -0400

    parse clean up

--- mega2_html/seo_parse_raw_wiki.py
+++ mega2_html/seo_parse_raw_wiki.py
@@ -10,9 +10,9 @@ import pdb
 ANALYSIS = "wik"
 PARSED   = "parsed_wik"
 
-HEAD = re.compile("""\|\s*(.*?)<ref""")
-HED  = re.compile("""\|\s*(.*?)\s*\|\|""")
-#REF  = re.compile("""(<ref.*?</ref>)|(<ref.*?/>)""")
+TOPIC = re.compile("""\|\s*(.*?)<ref""")
+TOPC  = re.compile("""\|\s*(.*?)\s*\|\|""")
+
 REF  = re.compile("""(<ref.*?)(</ref>|/>)""")
 CITE = re.compile("""<ref(.*?)/?>{{(.*?)}}""")
 REUS = re.compile("""<ref(.*?)/?>(\s*)""")
@@ -22,18 +22,75 @@ URL  = re.compile("""\[(.*?)\]""")
 DB   = {}
 DBREF= {}
 
+def get_citations(topic, line):
+    abbrev = topic.split()[0]
+    cnt = 0
+    for mi in REF.finditer(line):        # <ref>(full)</ref> vs <ref (reuse) />
+        cnt = cnt + 1
+        if mi.group(2) == '</ref>':
+            m = CITE.match(mi.group(1))  # define a full citation
+            fresh = True
+        else:
+            m = REUS.match(mi.group(0))  # reuse/name an old citation
+            fresh = False
+        if m is None:
+            print("Bad citation: {0}".format(ref))
+            continue
+
+        refclause = m.group(1).split("=")     # define reference name
+        if len(refclause) >= 2:
+            refkey = refclause[-1].strip() + '_cit'
+            if refkey in DBREF and fresh:
+                cnt += 1
+                refkey = abbrev + str(cnt) + '_cit'
+        elif cnt == 1:               # use 1st word of topic as reference name
+            refkey = abbrev + '_cit'
+        else:                            # and now there are multiple unnamed refs
+            refkey = abbrev + str(cnt) + '_cit'
+#           cnt += 1
+
+        refhash = {}
+        if refkey not in DBREF:
+            DBREF[refkey] = refhash
+        DB[topic][0].append(refkey)
+
+        ref = m.group(2)
+        if ref is None or ref == "":
+            continue
+
+        fields = ref.split("|")          # | splits fields
+        for field in fields:
+            field = field.strip()
+            kv = field.split("=", 1)
+            if len(kv) < 2:
+                if field != 'cite journal':  # no =; but this is how a citation begins
+                    print("{0} discarding cit element \"{1}\"".format(topic, field))
+                continue
+            refhash[kv[0]] = kv[1]
+
+def get_urls(topic, line):
+    for m in URL.finditer(line):
+        url = m.group(1)
+        if url.startswith("["): continue
+        fields = url.split(None, 1)
+        fields.reverse()
+        fields[0] = fields[0] + '_url'
+        DBREF[fields[0]] = fields[1]
+        DB[topic][1].append(fields[0])
+
 def parse_wiki(FILE, Parsed):
-#   pdb.set_trace()
     if not os.path.isfile(FILE):
         print("Not a File: {0}".format(FILE))
     File = io.open(FILE, "r", encoding='utf8')
     On = False
+    topic = None
     for line in File:
-        Force = False
         if not On:
             if line.startswith("'''Mega2, the Manipulation Environment for Genetic Analysis'''"):
-                Force = True
-                head = "Mega2 Prologue"
+                topic = "Mega2 Prologue"
+                DB[topic] = ([], [])  # no access to url list
+                get_citations(topic, line)
+                continue
             elif 'Input file formats' in line  and '==' in line:  # start
                 On = True
                 continue
@@ -44,77 +101,33 @@ def parse_wiki(FILE, Parsed):
         # important lines begin |
         if 'Mega2 documentation' in line  and '==' in line:       # end
             break
-        if line.startswith("||"):    # url
-            pass
-        elif line.startswith("|-"):   # ??
-            pass
-        elif line.startswith("|") or Force:   # citation
-            reflist = []
-            urllist = []
-            if not Force:
-                m = HEAD.search(line)    # | <header name> then "<ref ..."
-                if m is None:
-                    m = HED.search(line) # | <header name> then ... then "||"
-                    if m is None:
-                        if line != '|}':  # seems to happen occasionally
-                            print("No Name: {0}".format(line))
-                        continue
-                    else:    # no citation
-                        head = m.group(1)
-                        DB[head] = (reflist, urllist)
-                        print("No citation: {0}, viz \"{1}\"".format(head, line))  # tag only has urls
-                else:
-                    head = m.group(1)
-            headcnt = 0
-            DB[head] = (reflist, urllist)
-            for mi in REF.finditer(line):        # <ref>(full)</ref> vs <ref (reuse) />
-                headcnt = headcnt + 1
-                if mi.group(2) == '</ref>':
-                    m = CITE.match(mi.group(1))  # define a full citation
-                    fresh = True
-                else:
-                    m = REUS.match(mi.group(0))  # reuse/name an old citation
-                    fresh = False
-                if m is None:
-                    print("Bad citation: {0}".format(ref))
-                    continue
-                refhash = {}
-                refn = m.group(1).split("=")     # define reference name
-                if len(refn) >= 2:
-                    key = refn[-1].strip() + '_cit'
-                    if key in DBREF and fresh:
-                        headcnt += 1
-                        key = head.split()[0]+ str(headcnt) + '_cit'
-                elif headcnt == 1:               # use 1st word of head as reference name
-                    key = head.split()[0] + '_cit'
-                else:                            # and now there are multiple unnamed refs
-                    key = head.split()[0]+ str(headcnt) + '_cit'
-                if key not in DBREF:
-                    DBREF[key] = refhash
-                reflist.append(key)
-                ref = m.group(2)
-                if ref is None or ref == "":
-                    continue
-                fields = ref.split("|")          # | splits fields
-                for field in fields:
-                    field = field.strip()
-                    kv = field.split("=", 1)
-                    if len(kv) < 2:
-                        if field != 'cite journal':  # no =; but this is how a citation begins
-                            print("{0} discarding cit element \"{1}\"".format(head, field))
-                        continue
-                    refhash[kv[0]] = kv[1]
-        # look for urls in [...]
-        for m in URL.finditer(line):
-            url = m.group(1)
-            if url.startswith("["): continue
-            fields = url.split(None, 1)
-            fields.reverse()
-            fields[0] = fields[0] + '_url'
-            DBREF[fields[0]] = fields[1]
-            DB[head][1].append(fields[0])
 
-    fd = io.open(Parsed, "w", encoding='utf8')
+        if line.startswith("|-"):   # ??
+            pass
+
+        elif line.startswith("||"):   # url
+            get_urls(topic, line)     # topic on previous line (or here)
+
+        elif line.startswith("|"):    # citation
+
+            m = TOPIC.search(line)    # viz. | <topic name> "<ref ..."
+            if m:
+                topic = m.group(1)
+                DB[topic] = ([], [])
+                get_citations(topic, line)
+                get_urls(topic, line)
+            else:
+                m = TOPC.search(line)  # viz. | <topic name> ... "||"
+                if m:
+                    topic = m.group(1)
+                    DB[topic] = ([], [])
+                    print("No citation: {0}, viz \"{1}\"".format(topic, line))  # tag only has urls
+                    get_urls(topic, line)
+                else:
+                    if line != '|}':   # seems to happen occasionally
+                        print("No Name: {0}".format(line))
+
+def write_DBREF(fd):
     for k in sorted(DBREF.keys()):
         print("{0}: {1!r}".format('cit', k), file=fd)
         v = DBREF[k]
@@ -123,22 +136,20 @@ def parse_wiki(FILE, Parsed):
         else:
             for cik in sorted(v.keys()):
                 civ = v[cik]
-                if cik == 'pages':
-                    civ = civ.replace(u'\u2013', '-')
                 print("    {0}: {1!r}".format(cik, civ), file=fd)
             print("", file=fd)
 
+def write_DB(fd):
     for k in sorted(DB.keys()):
         print("{0}: {1!r}".format('name', k), file=fd)
-        v = DB[k][1]
-        for e in v:
-            print("{0!r} ".format(e), end='', file=fd)
-        v = DB[k][0]
-        for e in v:
-            print("{0!r} ".format(e), end='', file=fd)
-        print("\n", file=fd)
-    fd.close()
 
+        for e in DB[k][1]:  # url
+            print("{0!r} ".format(e), end='', file=fd)
+
+        for e in DB[k][0]:  # citation
+            print("{0!r} ".format(e), end='', file=fd)
+
+        print("\n", file=fd)
 
 def main():
     parse = OptionParser()
@@ -151,5 +162,9 @@ def main():
 
     parse_wiki(options.input, options.output)
 
+    fd = io.open(options.output, "w", encoding='utf8')
+    write_DBREF(fd)
+    write_DB(fd)
+    fd.close()
 
 main()
