5241d29b7ae95f64d302d079724c02d952c8c363
R. V. Baron <rvb5@pitt.edu>
2015-09-15 17:12:18 -0400

    oops forgot these

---
+++ srcdir/read_impute_bgen2.cpp
@@ -0,0 +1,301 @@
+/*
+  Mega2: Manipulation Environment for Genetic Analysis
+  Copyright (C) 1999-2015 Robert Baron, Charles P. Kollar,
+  Nandita Mukhopadhyay, Lee Almasy, Mark Schroeder, William P. Mulvihill,
+  Daniel E. Weeks, and University of Pittsburgh
+
+  This file is part of the Mega2 program, which is free software; you
+  can redistribute it and/or modify it under the terms of the GNU
+  General Public License as published by the Free Software Foundation;
+  either version 3 of the License, or (at your option) any later
+  version.
+
+  Mega2 is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+  for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+  For further information contact:
+      Daniel E. Weeks
+      e-mail: weeks@pitt.edu
+
+===========================================================================
+*/
+
+
+using namespace std;
+
+#include <stdio.h>
+
+#include <string.h>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <zlib.h>
+
+#include "genfile/bgen/bgen.hpp"
+
+#include "common.h"
+#include "tod.hh"
+#include "error_messages_ext.h"
+extern void           Exit(int arg, const char *file, const int line, const char *err);
+#include "typedefs.h"
+#include "fcmap_ext.h"
+#include "batch_input.h"
+#include "batch_input_ext.h"
+#include "mrecode.h"
+#include "mrecode_ext.h"
+#include "reorder_loci_ext.h"
+#include "plink_ext.h"
+#include "read_files_ext.h"
+#include "annotated_ped_file_ext.h"
+#include "annotated_ped_file.h"
+
+#include "input_ops.hh"
+#include "read_impute.hh"
+#include "read_impute_bgen2.hh"
+
+
+SECTION_ERR_INIT(bad_marker_name);
+void ReadBgen2::read_input_file()
+{
+    BgenParser bgen;
+    bgen.m_filename = impute_file;
+    bgen.rip = this;
+
+    bgen.read_input_file();
+}
+
+void ReadBgen2::build_genotypes(linkage_locus_top *LTop, annotated_ped_rec *persons)
+{
+    BgenParserGenotypeReadHelper gh;
+    gh.m_filename = impute_file;
+    gh.rip = this;
+
+    build_internal_genotypes(LTop, persons, gh);
+}
+
+void BgenParserGenotypeReadHelper::genotypes_init()
+{
+//  asm("int $3");
+    open();
+    summarise( std::cerr ) ;
+    get_sample_ids( [this]( std::string const& id ) { } );
+
+/*
+    open(C(impute_file));
+    pass = 1;
+    read_header();
+    pass = 2;
+    if (layout < 2) {
+        block.cdata = new unsigned char[6 * samples];
+        block.rdata = new unsigned char[6 * samples];
+        if (layout == 0)
+            scale = 10000;
+        else if (layout == 1)
+            scale = 32768;
+    } else if (layout == 2) {
+    } else {
+//      ERROR
+    }
+*/
+}
+
+boolean BgenParserGenotypeReadHelper::genotypes_marker_hdr(int mrk_idx, string& hmm, string& rsid, string& pos,
+                                                     const char* &A, const char* &B)
+{
+    bool ret;
+    string chromosome;
+    uint32_t position;
+    vector<string> alleles;
+
+    ret = read_variant( &chromosome, &position, &rsid, &alleles );
+
+    hmm = "---";
+    fix_marker_pos(pos, position);
+    A = C(alleles[0]);
+    B = C(alleles[1]);
+
+    prob_sample = 0;
+    return ret;
+/*
+    if (layout == 0) {
+        process_10(mrk_idx);
+    } else if (layout == 1) {
+        process_11(mrk_idx);
+    } else if (layout == 2) {
+        process_12(mrk_idx);
+    }
+
+    if ((flags & compressF) > 0)
+        zp = block.cdata;
+    else
+        zp = 0;
+
+    hmm = "---";
+    rsid = block.rsid;
+    fix_marker_pos(pos);
+    A = C(block.allele[0]);
+    B = C(block.allele[1]);
+
+*/
+}
+
+void BgenParserGenotypeReadHelper::genotypes_skip()
+{
+    ignore_probs();
+}
+
+void BgenParserGenotypeReadHelper::genotypes_sample_prob(Token::d3& nums)
+{
+    if (prob_sample == 0)
+        read_probs(&probs);
+
+    nums[0] = probs[prob_sample][0];
+    nums[1] = probs[prob_sample][1];
+    nums[2] = probs[prob_sample][2];
+    
+    prob_sample++;
+
+    if (prob_sample == m_context.number_of_samples) {
+        prob_sample = 0;
+    }
+/*
+    nums[0] = ((double)read_ushort(zp)) / scale;
+    nums[1] = ((double)read_ushort(zp)) / scale;
+    nums[2] = ((double)read_ushort(zp)) / scale;
+*/
+}
+
+void BgenParserGenotypeReadHelper::genotypes_end()
+{
+    close();
+/*
+    if (layout < 2) {
+        delete [] block.cdata;
+        delete [] block.rdata;
+    }
+*/
+}
+
+
+void BgenParser::fix_marker_pos(string& ccpos, unsigned long pos)
+{
+    char cpos[10];
+    sprintf(cpos, "%ld", pos);
+    ccpos = string(cpos);
+}
+
+void BgenParser::validate_marker_name(string& rsid, string& chrm, string& rsid_field, const string& ccpos)
+{
+    VecsDB fields;
+    split(fields, rsid_field, ":", 3);
+
+    if (fields.size() == 1) {
+        rsid = fields[0];
+        if (rsid == "." || rsid == "NA" || rsid == "na") {
+            chrm = rip->oxford_single_chr;
+            rsid = "chr" + rip->oxford_single_chr + "_" + ccpos;
+        }
+    } else {
+        if (fields[0].compare(0, 3, "chr") == 0)
+            fields[0].erase(0, 3);
+        if (inMap(fields[0], Input->G.chrm_set)) {
+            chrm = fields[0];
+            rsid = "chr" + fields[0] + "_" + ccpos;
+        } else if (fields[0].compare(0, 2, "rs") == 0 && rip->oxford_single_chr != "--") {
+            chrm = rip->oxford_single_chr;
+            rsid = fields[0];
+        } else {
+            SECTION_ERR(bad_marker_name);
+            errorvf("impute2 file: bad marker name %s\n", C(rsid_field));
+            badname++;
+        }
+    }
+}
+
+// This example program reads data from a bgen file specified as the first argument
+// and outputs it as a VCF file.
+void BgenParser::read_input_file()
+{
+    asm("int $3");
+    try {
+        open( ) ;
+
+        rip->markers_all      = m_context.number_of_variants;
+        rip->markers_filtered = m_context.number_of_variants;
+
+        rip->people_all       = m_context.number_of_samples;
+        rip->people_filtered  = m_context.number_of_samples;
+
+        summarise( std::cerr ) ;
+
+/*
+        // Output header
+        std::cout << "##fileformat=VCFv4.2\n"
+            << "FORMAT=<ID=GP,Type=Float,Number=G,Description=\"Genotype call probabilities\">\n"
+            << "#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT" ;
+        get_sample_ids(
+            []( std::string const& id ) { std::cout << "\t" << id ; }
+        ) ;
+        std::cout << "\n" ;
+*/        
+        get_sample_ids(
+            [this]( std::string const& id ) { sample.samples.push_back(id) ; }
+            );
+        
+        // Output variants
+        std::string chromosome ;
+        uint32_t position ;
+        std::string rsid ;
+        std::vector< std::string > alleles ;
+//        std::vector< std::vector< double > > probs ;
+
+    
+        while( read_variant( &chromosome, &position, &rsid, &alleles )) {
+/*
+              std::cout << chromosome << '\t'
+                << position << '\t'
+                << rsid << '\t' ;
+            for( std::size_t i = 0; i < alleles.size(); ++i ) {
+                std::cout << ( i > 0 ? "," : "" ) << alleles[i] ;
+            }
+            std::cout << "\t.\t.\t.\tGP" ;
+*/
+            string ccpos;
+            fix_marker_pos(ccpos, position);
+
+            string rsid_name, chrm;
+            validate_marker_name(rsid_name, chrm, rsid, ccpos);
+            rip->markers.push_back(new ImpMarker(rsid_name, chrm, ccpos, alleles[0], alleles[1], rip->read_info));
+
+            ignore_probs() ;
+/*
+            read_probs( &probs ) ;
+
+            for( std::size_t i = 0; i < probs.size(); ++i ) {
+              std::cout << '\t' ;
+                for( std::size_t j = 0; j < probs[i].size(); ++j ) {
+                    std::cout << ( j > 0 ? "," : "" ) ;
+                    if( probs[i][j] == -1 ) {
+                        std::cout << "." ;
+                    } else {
+                        std::cout << probs[i][j] ;
+                    }
+                }
+            }
+          std::cout << "\n" ;
+*/
+        }
+//        return 0 ;
+    }
+    catch( genfile::bgen::BGenError const& e ) {
+        std::cerr << "!! Uh-oh, error parsing bgen file.\n" ;
+	throw;
+//        return -1 ;
+    }
+}
---
+++ srcdir/read_impute_bgen2.hh
@@ -0,0 +1,287 @@
+/*
+  Mega2: Manipulation Environment for Genetic Analysis
+  Copyright (C) 1999-2015 Robert Baron, Charles P. Kollar,
+  Nandita Mukhopadhyay, Lee Almasy, Mark Schroeder, William P. Mulvihill,
+  Daniel E. Weeks, and University of Pittsburgh
+
+  This file is part of the Mega2 program, which is free software; you
+  can redistribute it and/or modify it under the terms of the GNU
+  General Public License as published by the Free Software Foundation;
+  either version 3 of the License, or (at your option) any later
+  version.
+
+  Mega2 is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+  for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+  For further information contact:
+      Daniel E. Weeks
+      e-mail: weeks@pitt.edu
+
+===========================================================================
+*/
+
+#ifndef READ_IMPUTE_BGEN2_HH
+#define READ_IMPUTE_BGEN2_HH
+
+#include <iostream>
+#include <fstream>
+#include <memory>
+#include <stdexcept>
+#include "genfile/bgen/bgen.hpp"
+
+#include "read_impute.hh"
+
+class ReadBgen2 : public ReadImputed {
+public:
+    virtual void read_input_file();
+    virtual void build_genotypes(linkage_locus_top *LTop, annotated_ped_rec *persons);
+};
+
+// ProbSetter is a callback object appropriate
+// for passing to bgen::read_genotype_data_block().
+// See the comment about bgen::read_genotype_data_block() for more info.
+// Its purpose is to store genotype probability values in the desired
+// data structure (which here is a vector of vectors of doubles).
+class ProbSetter {
+public:
+    typedef std::vector< std::vector< double > > Data ;
+    ProbSetter( Data* result ):
+        m_result( result ),
+        m_sample_i(0)
+    {}
+        
+    // Called once allowing us to set storage.
+    void initialise( std::size_t number_of_samples, std::size_t number_of_alleles ) {
+        m_result->clear() ;
+        m_result->resize( number_of_samples ) ;
+    }
+    
+    // Called once per sample to determine whether we want data for this sample
+    bool set_sample( std::size_t i ) {
+        m_sample_i = i ;
+        // Yes, here we want info for all samples.
+        return true ;
+    }
+    
+    // Called once per sample to set the number of probabilities that are present.
+    void set_number_of_entries(
+        std::size_t number_of_entries,
+        genfile::OrderType order_type,
+        genfile::ValueType value_type
+    ) {
+        assert( value_type == genfile::eProbability ) ;
+        m_result->at( m_sample_i ).resize( number_of_entries ) ;
+        m_entry_i = 0 ;
+    }
+
+    void operator()( double value ) {
+        m_result->at( m_sample_i ).at( m_entry_i++ ) = value ;
+    }
+
+    void operator()( genfile::MissingValue value ) {
+        // Here we encode missing probabilities with -1
+        m_result->at( m_sample_i ).at( m_entry_i++ ) = -1 ;
+    }
+
+private:
+    Data* m_result ;
+    std::size_t m_sample_i ;
+    std::size_t m_entry_i ;
+} ;
+
+// BgenParser is a thin wrapper around the core functions in genfile/bgen/bgen.hpp.
+// This class tracks file state and handles passing the right callbacks.
+class BgenParser {
+public:    
+    BgenParser():
+        m_filename( "" ),
+        m_state( e_NotOpen ),
+        m_have_sample_ids( false ) {}
+
+    void open( )
+    {
+        // Open the stream
+/*
+        m_stream.reset(
+            new std::ifstream( filename, std::ifstream::binary )
+        ) ;
+*/
+        m_stream = new std::ifstream( C(m_filename), std::ifstream::binary );
+        if( !*m_stream ) {
+            throw std::invalid_argument( m_filename ) ;
+        }
+        m_state = e_Open ;
+
+        // Read the offset, header, and sample IDs if present.
+        genfile::bgen::read_offset( *m_stream, &m_offset ) ;
+        genfile::bgen::read_header_block( *m_stream, &m_context ) ;
+        if( m_context.flags & genfile::bgen::e_SampleIdentifiers ) {
+            genfile::bgen::read_sample_identifier_block(
+                *m_stream, m_context,
+                [this]( std::string id ) { m_sample_ids.push_back( id ) ; }
+            ) ;
+            m_have_sample_ids = true ;
+        }
+        
+        // Jump to the first variant data block.
+        m_stream->seekg( m_offset + 4 ) ;
+
+        // We keep track of state (though it's not really needed for this implementation.)
+        m_state = e_ReadyForVariant ;
+
+    }
+
+    void close() {
+        m_stream->close();
+        delete m_stream;
+    }
+
+    std::ostream& summarise( std::ostream& o ) const {
+        o << "BgenParser: bgen file ("
+            << ( m_context.flags & genfile::bgen::e_v12Layout ? "v1.2 layout" : "v1.1 layout" )
+            << ", "
+            << ( m_context.flags & genfile::bgen::e_CompressedSNPBlocks ? "compressed" : "uncompressed" ) << ")"
+            << " with " 
+            << m_context.number_of_samples << " samples and "
+            << m_context.number_of_variants << " variants.\n" ;
+        return o ;
+    }
+
+    std::size_t number_of_samples() const {
+        return m_context.number_of_samples ;
+    }
+
+    // Report the sample IDs in the file using the given setter object
+    // (If there are no sample IDs in the file, we report a dummy identifier).
+    template< typename Setter >
+    void get_sample_ids( Setter setter ) {
+        sample.count = m_context.number_of_samples;
+        if( m_have_sample_ids ) {
+            for( std::size_t i = 0; i < m_context.number_of_samples; ++i ) {
+                setter( m_sample_ids[i] ) ;
+            }
+        } else {
+            for( std::size_t i = 0; i < m_context.number_of_samples; ++i ) {
+                setter( "(unknown_sample_" + std::to_string( i+1 ) + ")" ) ;
+            }
+        }
+    }
+
+    // Attempt to read identifying information about a variant from the bgen file, returning
+    // it in the given fields.
+    // If this method returns true, data was successfully read, and it should be safe to call read_probs()
+    // or ignore_probs().
+    // If this method returns false, data was not successfully read indicating the end of the file.
+    bool read_variant(
+        std::string* chromosome,
+        uint32_t* position,
+        std::string* rsid,
+        std::vector< std::string >* alleles
+    ) {
+        assert( m_state == e_ReadyForVariant ) ;
+        std::string SNPID ; // read but ignored in this toy implementation
+        
+        if(
+            genfile::bgen::read_snp_identifying_data(
+                *m_stream, m_context,
+                &SNPID, rsid, chromosome, position,
+                [&alleles]( std::size_t n ) { alleles->resize( n ) ; },
+                [&alleles]( std::size_t i, std::string const& allele ) { alleles->at(i) = allele ; }
+            )
+        ) {
+            m_state = e_ReadyForProbs ;
+            return true ;
+        } else {
+            return false ;
+        }
+    }
+    
+    // Read genotype probability data for the SNP just read using read_variant()
+    // After calling this method it should be safe to call read_variant() to fetch
+    // the next variant from the file.
+    void read_probs( std::vector< std::vector< double > >* probs ) {
+        assert( m_state == e_ReadyForProbs ) ;
+        ProbSetter setter( probs ) ;
+        genfile::bgen::read_and_parse_genotype_data_block< ProbSetter >(
+            *m_stream,
+            m_context,
+            setter,
+            &m_buffer1,
+            &m_buffer2
+        ) ;
+        m_state = e_ReadyForVariant ;
+    }
+
+    // Ignore genotype probability data for the SNP just read using read_variant()
+    // After calling this method it should be safe to call read_variant()
+    // to fetch the next variant from the file.
+    void ignore_probs() {
+        genfile::bgen::ignore_genotype_data_block( *m_stream, m_context ) ;
+        m_state = e_ReadyForVariant ;
+    }
+
+    void fix_marker_pos(std::string& ccpos, unsigned long pos);
+    void validate_marker_name(std::string& rsid, std::string& chrm, std::string& rsid_field, const std::string& ccpos);
+    void read_input_file();
+
+    std::string m_filename ;
+    ReadImputed *rip;
+    unsigned long badname;
+
+    std::vector< std::vector< double > > probs ;
+    unsigned int prob_sample;
+
+    struct Sampleblock {
+        unsigned count;
+        VecsDB   samples;
+    } sample;
+
+private:
+//rvb    std::unique_ptr< std::ifstream > m_stream ;
+//rvb    std::auto_ptr< std::fstream > m_stream ;
+        std::ifstream *m_stream ;
+
+protected:
+    // bgen::Context object holds information from the header block,
+    // including bgen flags
+    genfile::bgen::Context m_context ;
+
+private:
+    // offset byte from top of bgen file.
+    uint32_t m_offset ;
+
+    // We keep track of our state in the file.
+    // Not strictly necessary for this implentation but makes it clear that
+    // calls must be read_variant() followed by read_probs() (or ignore_probs())
+    // repeatedly.
+    enum State { e_NotOpen = 0, e_Open = 1, e_ReadyForVariant = 2, e_ReadyForProbs = 3, eComplete = 4 } ;
+    State m_state ;
+
+    // If the BGEN file contains samples ids, they will be read here.
+    bool m_have_sample_ids ;
+    std::vector< std::string > m_sample_ids ;
+    
+    // Buffers, these are used as working space by bgen implementation.
+    std::vector< char > m_buffer1, m_buffer2 ;
+} ;
+
+
+class BgenParserGenotypeReadHelper : public GenotypeReadHelper, public BgenParser
+{
+public:
+    virtual void genotypes_init();
+    virtual boolean genotypes_marker_hdr(int mrk_idx, std::string& hmm, std::string& rsid, std::string& pos,
+                                         const char* &A, const char* &B);
+    virtual void genotypes_skip();
+    virtual void genotypes_sample_prob(Token::d3& nums);
+    virtual void genotypes_end();
+};
+
+
+#endif
