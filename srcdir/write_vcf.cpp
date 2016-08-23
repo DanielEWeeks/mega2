05c7c767cb49daa1cc78516805d8e985225edf2d
Justin <jrs110@pitt.edu>
2016-08-23 14:23:03 -0400

    Updates to the VCF output process.

--- srcdir/write_vcf.cpp
+++ srcdir/write_vcf.cpp
@@ -43,6 +43,7 @@
 #include "omit_ped_ext.h"
 #include "output_file_names_ext.h"
 #include "user_input_ext.h"
+#include "write_files_ext.h"
 
 #include "write_vcf_ext.h"
 
@@ -81,13 +82,67 @@ void CLASS_VCF::create_output_file(linkage_ped_top *LPedTreeTop, analysis_type *
     write_VCF_file(Top, file_name_stem,file_names ,pwid, fwid);
     write_VCF_ped(Top, file_name_stem, file_names ,pwid, fwid);
     //we only want a phenotype file if we have more than one trait, the first trait is always put into the pedigree fam file by convention
-    //if(num_traits>1)
+    if(num_traits>1)
         write_VCF_pheno(Top, file_name_stem, file_names ,pwid, fwid);
     write_VCF_sh(Top, file_name_stem, file_names);
 }
 
+
+//CHROM POS ID REF ALT QUAL FILTER INFO FORMAT 1_1 1_2 2_1
+//20 14370 rs6054257 G A 29 PASS NS=3;DP=14;AF=0.5;DB;H2 GT:GQ:DP:HQ 0|0:48:1:51,51 1|0:48:8:51,51 1/1:43:5:.,.
 void CLASS_VCF::write_VCF_file(linkage_ped_top *Top, const char *prefix, char *file_names[], const int pwid, const int fwid){
-    //need a second loop to make header?? one of just ped_per
+    vlpCLASS(vcf_vcfs_header,chr,ped_per) {
+        vlpCTOR(vcf_vcfs_header, chr, ped_per) { }
+        typedef char *str;
+        str *file_names;
+
+        void file_loop() {
+            mssgvf("        VCF format file:      %s/%s\n", *_opath, file_names[0]);
+            data_loop(*_opath, file_names[0], "w");
+        }
+
+        void file_header() {
+            pr_printf("##fileformat=VCFv4.2\n");
+            pr_printf("##filedate=%s\n", __DATE__);
+            pr_printf("##source=MEGA2\n");
+            pr_printf("##INFO=<ID=AF,Number=.,Type=Float,Description=\"Allele Frequency\">\n");
+            pr_printf("##INFO=<ID=GC,Number=G,Type=Integer,Description=\"Genotype Counts\">\n");
+            pr_printf("##INFO=<ID=NS,Number=1,Type=Integer,Description=\"Number of Samples With Data\">\n");
+            pr_printf("##FORMAT=<ID=GT,Number=1,Type=String,Description=\"Genotype\">\n");
+            pr_printf("##FILTER=<ID=PASS,Description=\"Passed variant FILTERs\">\n");
+//            pr_printf("%-7s","#CHROM");
+//            pr_printf("%-10s","POS");
+//            pr_printf("%-15s","ID");
+//            pr_printf("%-6s","REF");
+//            pr_printf("%-6s","ALT");
+//            pr_printf("%-5s","QUAL");
+//            pr_printf("%-7s","FILTER");
+//            pr_printf("%-65s","INFO");
+//            pr_printf("%-7s","FORMAT");
+            pr_printf("#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\t");
+        }
+
+        void inner(){
+            pr_fam();
+            pr_printf("_");
+            pr_per();
+            pr_printf(" ");
+        }
+
+//        void filep_close(){
+//            pr_nl();
+//        }
+
+    } *hlp = new vcf_vcfs_header(Top);
+
+    hlp->setfln(prefix, ".vcf");
+    hlp->file_names = file_names;
+
+    hlp->load_formats_no_space(-1);
+    //lp->load_formats(fwid, pwid, -1);
+
+    hlp->iterate();
+
 
 
     vlpCLASS(vcf_vcfs,chr,loci_ped_per) {
@@ -95,64 +150,80 @@ void CLASS_VCF::write_VCF_file(linkage_ped_top *Top, const char *prefix, char *f
         typedef char *str;
         str *file_names;
         int firstrun;
-        //linkage_locus_top *LTop = Top->LocusTop;
+        linkage_locus_top *LTop ;
 
         void file_loop() {
-            mssgvf("        VCF format file:      %s/%s\n", *_opath, file_names[0]);
-            data_loop(*_opath, file_names[0], "w");
+            //mssgvf("        VCF format file:      %s/%s\n", *_opath, file_names[0]);
+            data_loop(*_opath, file_names[0], "a");
         }
         //here we can put the VCF header data
         void file_header() {
-            pr_printf("##fileformat=VCFv4.2\n");
-            pr_printf("##filedate=%s\n", __DATE__);
-            pr_printf("##source=MEGA2\n");
-            pr_printf("##INFO=<ID=AF,Number=.,Type=Float,Description=\"Allele Frequency\">");
-            pr_printf("##INFO=<ID=GC,Number=G,Type=Integer,Description=\"Genotype Counts\">");
-            pr_printf("##INFO=<ID=NS,Number=1,Type=Integer,Description=\"Number of Samples With Data\">");
-            pr_printf("##FORMAT=<ID=GT,Number=1,Type=String,Description=\"Genotype\">");
-            pr_printf("##FILTER=<ID=PASS,Description=\"Passed variant FILTERs\">\n");
-            pr_printf("#CHROM    POS    ID    REF    ALT    QUAL    FILTER    INFO    FORMAT    ");
-
-            //pr_printf("#CHRO\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\n");
-
-            firstrun = 1;
-
+            //want this here since the new line at filep_close() didn't seem to do the trick
+            pr_nl();
         }
 
         void inner() {
-            if(firstrun){
-                pr_fam();
-                pr_printf("_");
-                pr_per();
-                //pr_printf("%d_%d    ",);
-                firstrun = 0;
-            }
+            str a1, a2;
+            if (_allele1 == 1)
+                a1 = strdup("0");
+            else if (_allele1 == 2)
+                a1 = strdup("1");
             else
-                pr_marker();
+                a1 = strdup(".");
+
+            if (_allele2 == 1)
+                a2 = strdup("0");
+            else if (_allele2 == 2)
+                a2 = strdup("1");
+            else
+                a2 = strdup(".");
+
+            pr_printf("%s/%s\t", a1, a2);
+            //pr_printf("%s/%s\t", format_allele(_tlocusp, _allele1), format_allele(_tlocusp, _allele1));
+            //pr_printf("%s/%s ", format_allele(_tlocusp, _allele1), format_allele(_tlocusp, _allele1));
+            //pr_printf("%d/%d    ",_allele1,_allele2);
 
         }
 
 
-        //goal for inner loop:
-        //#CHROM POS ID REF ALT QUAL FILTER INFO FORMAT 1_1 1_2 2_1
-        //20 14370 rs6054257 G A 29 PASS NS=3;DP=14;AF=0.5;DB;H2 GT:GQ:DP:HQ 0|0:48:1:51,51 1|0:48:8:51,51 1/1:43:5:.,.
-        void loci_start() {
-            pr_printf("%d    ", _numchr);
-            pr_physical_distance(0);
-            pr_printf("    ");
-            pr_marker_name();
-            pr_printf("    ");
-            pr_marker_alleles();
-            pr_printf("    ");
-            //info line, we want Allele frequency, genotype count, and # of samples
-            pr_printf("AF=%s;GC=%s,%s,%s;NS=%s;","frequency","count1","count2","count3","number");
-            pr_printf("    ");
-            pr_printf("%s","PASS");
-            pr_printf("    ");
-            pr_printf("%s",".");
-            pr_printf("    ");
-            pr_nl();
 
+        void loci_start() {
+            pr_printf("%d\t", _numchr);
+            pr_physical_distance(0);
+            pr_printf("\t");
+            pr_printf("%s\t",_tlocusp->LocusName);
+            str a1, a2;
+            if(!strcmp(_tlocusp->Allele[_allele1].AlleleName,"dummy"))
+                a1 = strdup(".");
+            else
+                a1 = strdup(_tlocusp->Allele[_allele1].AlleleName);
+
+            if(!strcmp(_tlocusp->Allele[_allele2].AlleleName,"dummy"))
+                a2 = strdup(".");
+            else
+                a2 = strdup(_tlocusp->Allele[_allele2].AlleleName);
+            pr_printf("%s\t%s\t",a1,a2);
+            pr_printf(".\t");
+            pr_printf("PASS\t");
+            //info line: genetic distance in centimorgans, a (triple float avg, male, female), Allele frequency (float), genotype count (triple of ints?), and # of samples for data (int)
+            pr_printf("CM=%.2f,%.2f,%.2f;AF=%.2f;GC=%s,%s,%s;NS=%d;\t",_tlocusp->Marker->pos_avg,_tlocusp->Marker->pos_male,_tlocusp->Marker->pos_female,_tlocusp->Allele[_allele2].Frequency,"count1","count2","count3",0);
+            pr_printf("GT\t");
+
+
+//            pr_printf("%-7d", _numchr);
+//            pr_physical_distance(0);
+//            pr_printf("%-1s","");
+//            pr_printf("%-15s",_tlocusp->LocusName);
+//            pr_printf("%-6s%-6s",_tlocusp->Allele[_allele1].AlleleName,_tlocusp->Allele[_allele2].AlleleName);
+//            pr_printf("%-5s",".");
+//            pr_printf("%-7s","PASS");
+//            //info line, we want Allele frequency, genotype count, and # of samples
+//            pr_printf("CM=%.2f,%.2f,%.2f;AF=%.2f;GC=%s,%s,%s;NS=%d%-6s ",_tlocusp->Marker->pos_avg,_tlocusp->Marker->pos_male,_tlocusp->Marker->pos_female,_tlocusp->Allele[_allele2].Frequency,"count1","count2","count3",_tlocusp->Marker->col_num,";");
+//            pr_printf("%-7s","GT");
+        }
+
+        void loci_end(){
+            pr_nl();
         }
     } *lp = new vcf_vcfs(Top);
 
@@ -165,6 +236,7 @@ void CLASS_VCF::write_VCF_file(linkage_ped_top *Top, const char *prefix, char *f
     lp->iterate();
 
     delete lp;
+    delete hlp;
 }
 
 //this will write the pedigree in the PLINK fam file format
