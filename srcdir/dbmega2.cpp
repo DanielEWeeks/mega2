3bba1e1cebe862f1ec38057e8929573c65a2d915
R. V. Baron <rvb5@pitt.edu>
2016-09-21 15:44:38 -0400

    mega2 will now either use the db or create it if it does not exist.  --dbfile <file> can set the name.

--- srcdir/dbmega2.cpp
+++ srcdir/dbmega2.cpp
@@ -311,10 +311,21 @@ void db_drop_all() {
 }
 
 int db_exists_db() {
-//  delete_file(DBfile);
+    char *dbf = DBfile;
 
-    if (DBfile[0] == 0)
-        return 0;
+    if (DBfile[0] == 0) { // not on commandline
+        BatchValueGet(dbf, "DBfile_name");
+    }
+
+    if (strchr(DBfile, '/')) {
+        /* leave alone*/;
+    } else if ( (database_dump && database_read) && Mega2OutputPath) {
+        int l = strlen(Mega2OutputPath);
+        memmove(DBfile+l+1, DBfile, strlen(DBfile)+1);
+        strcpy(DBfile, Mega2OutputPath);
+        DBfile[l] = '/';
+        BatchValueSet(dbf, "DBfile_name");
+    } 
 
     FILE *f = fopen(DBfile, "r");
     if (f == NULL) {
@@ -328,22 +339,6 @@ void db_open_db() {
     extern void delete_file(const char *);
     extern void add_sumdir(char *);
     if (!database_dump && !database_read) return;
-
-    if (DBfile[0] == 0) { // not on commandline
-        char *cp = DBfile;
-        BatchValueGet(cp, "DBfile_name");
-    }
-
-    if (strchr(DBfile, '/')) {
-        /* leave alone*/;
-    } else if ( (database_dump && database_read) && Mega2OutputPath) {
-        int l = strlen(Mega2OutputPath);
-        char *dbf = DBfile;
-        memmove(DBfile+l+1, DBfile, strlen(DBfile)+1);
-        strcpy(DBfile, Mega2OutputPath);
-        DBfile[l] = '/';
-        BatchValueSet(dbf, "DBfile_name");
-    } 
 
     if (database_dump) {
         if (db_exists_db()) {
