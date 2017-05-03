95cf5799668cfe3f9d510e6a2155d58661c12c0f
R. V. Baron <rvb5@pitt.edu>
2017-05-03 14:07:43 -0400

    bookkeeping

---
+++ srcdir/RR/tstvcf.R
@@ -0,0 +1,33 @@
+
+#   Mega2: Manipulation Environment for Genetic Analysis
+#   Copyright (C) 1999-2017 Robert Baron, Justin R. Stickel, Charles P. Kollar,
+#   Nandita Mukhopadhyay, Lee Almasy, Mark Schroeder, William P. Mulvihill,
+#   Daniel E. Weeks, and University of Pittsburgh
+#  
+#   This file is part of the Mega2 program, which is free software you
+#   can redistribute it and/or modify it under the terms of the GNU
+#   General Public License as published by the Free Software Foundation
+#   either version 3 of the License, or (at your option) any later
+#   version.
+#  
+#   Mega2 is distributed in the hope that it will be useful, but WITHOUT
+#   ANY WARRANTY without even the implied warranty of MERCHANTABILITY or
+#   FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+#   for more details.
+#  
+#   You should have received a copy of the GNU General Public License
+#   along with this program if not, write to the Free Software
+#   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#  
+#   For further information contact:
+#       Daniel E. Weeks
+#       e-mail: weeks@pitt.edu
+# 
+# ===========================================================================
+
+library(mega2vcf)
+
+ENV=read.Mega2DB("yj1.db", verbose = 1)
+
+Mega2VCF("a1", ENV$markers[ENV$markers$chromosome == 1,])
+
