f66993b0c352c77c8a7b993080a0718256c3bbf8
R. V. Baron <rvb5@pitt.edu>
2017-08-25 11:18:26 -0400

    revise copyright owner to Pitt with authors as contributors

--- GetRefAlleles.sh
+++ GetRefAlleles.sh
@@ -1,5 +1,34 @@
 #!/bin/csh -f
 
+#   Mega2: Manipulation Environment for Genetic Analysis.
+#  
+#   Copyright 2017, University of Pittsburgh. All Rights Reserved.
+#  
+#   Contributors to Mega2: Robert Baron, Justin R. Stickel, Charles P. Kollar,
+#   Nandita Mukhopadhyay, Lee Almasy, Mark Schroeder, William P. Mulvihill,
+#   and Daniel E. Weeks.
+#  
+#   This file is part of the Mega2 program, which is free software; you
+#   can redistribute it and/or modify it under the terms of the GNU
+#   General Public License as published by the Free Software Foundation;
+#   either version 3 of the License, or (at your option) any later
+#   version.
+#  
+#   Mega2 is distributed in the hope that it will be useful, but WITHOUT
+#   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+#   FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+#   for more details.
+#  
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, write to the Free Software
+#   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#  
+#   For further information contact:
+#       Daniel E. Weeks
+#       e-mail: weeks@pitt.edu
+# 
+# ===========================================================================
+
 #  GetRefAlleles.sh
 #  Allows the construction of a simple reference allele file
 #  The output will be of the format:
