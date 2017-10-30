39073c84602aaff1998da83a52f7759f94075ccf
Daniel E. Weeks <weeks@pitt.edu>
2017-10-30 10:38:34 -0400

    Made the applyFnToGenes example faster by commenting out the slowest part.

--- srcdir/R/Mega2R/R/mega2riterate.R
+++ srcdir/R/Mega2R/R/mega2riterate.R
@@ -303,7 +303,7 @@ read.Mega2DB = function(db, ...) {
 #'   }
 #'
 #'    # apply function "show" to all transcripts on genes ELL2 and CARD15
-#'    applyFnToGenes(show, genes_arg = c("CEP104"))
+#'    # applyFnToGenes(show, genes_arg = c("CEP104"))
 #'
 #'    # apply function "show" to all genotypes on chromosomes 11 for two base
 #'    # pair ranges
