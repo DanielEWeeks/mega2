b9440f277d029d659a27bd12d8f3a0e060483ba0
Justin <jrs110@pitt.edu>
2018-01-29 14:14:36 -0500

    Rewrite of the genotype code for reading bcfs

--- srcdir/annotated_ped_file.cpp
+++ srcdir/annotated_ped_file.cpp
@@ -1626,7 +1626,7 @@ static linkage_ped_top *read_common_ped_file(FILE *filep, char *pedfile,
     int check_ungenotyped = 0;
     int xcf = Input_Format == in_format_binary_VCF ||
 	      Input_Format == in_format_compressed_VCF ||
-              Input_Format == in_format_VCF ;
+              Input_Format == in_format_VCF;
 
     // allele_count should be == 0 when using a .ped file...
 
