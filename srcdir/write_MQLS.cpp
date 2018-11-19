e4f81fd94ab70eeb70fec045b384521658633bd2
Daniel E. Weeks <weeks@pitt.edu>
2018-11-19 17:44:31 -0500

    Adjusted MQLS-XM shell script to rename XMtest.* files when running on the X chromosome.

--- srcdir/write_MQLS.cpp
+++ srcdir/write_MQLS.cpp
@@ -532,10 +532,10 @@ void CLASS_MQLS::write_MQLS_shell_script(linkage_ped_top *Top, const char *prefi
             pr_printf("echo MQLS-XM Done.\n");
             pr_printf("echo Renaming MQLS-XM results.\n");
             if(_numchr == 23) {
-                pr_printf("mv MQLStest.out MQLS.X.out \n", _numchr);
-                pr_printf("mv MQLStest.top MQLS.X.top\n", _numchr);
-                pr_printf("mv MQLStest.testvalues MQLS.X.testvalues\n", _numchr);
-                pr_printf("mv MQLStest.pvalues MQLS.X.pvalues\n", _numchr);
+                pr_printf("mv XMtest.out MQLS.X.out \n", _numchr);
+                pr_printf("mv XMtest.top MQLS.X.top\n", _numchr);
+                pr_printf("mv XMtest.testvalues MQLS.X.testvalues\n", _numchr);
+                pr_printf("mv XMtest.pvalues MQLS.X.pvalues\n", _numchr);
             }
             else if(_numchr<10) {
                 pr_printf("mv MQLStest.out MQLS.0%d.out \n", _numchr);
