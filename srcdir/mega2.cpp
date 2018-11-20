1760d37dcecf3e1ce0b7b094ab10e60083434a2d
Justin <jrs110@pitt.edu>
2018-11-20 09:59:39 -0500

    Allowed MQLS to write/read batch values for genetic_distance_index and genetic_distance_sex_type_map

--- srcdir/mega2.cpp
+++ srcdir/mega2.cpp
@@ -1236,10 +1236,20 @@ int             main(int argc, char **argv, char **env)
 
         distance_init_dump(LPedTreeTop, &analysis);
     }else if(database_read && analysis == MQLS) {
-        genetic_distance_index = -1;
-        genetic_distance_sex_type_map = -1;
-        distance_init_dump(LPedTreeTop,&analysis);
-        //get_genetic_distance_index(LPedTreeTop->EXLTop); 
+        if(!ITEM_READ(Value_Genetic_Distance_Index)) {
+            genetic_distance_index = -1;
+            genetic_distance_sex_type_map = -1;
+        }
+        else {
+            BatchValueGet(genetic_distance_index,"Value_Genetic_Distance_Index");
+            BatchValueGet(genetic_distance_sex_type_map,"Value_Genetic_Distance_SexTypeMap");
+        }
+        distance_init_dump(LPedTreeTop, &analysis);
+        //get_genetic_distance_index(LPedTreeTop->EXLTop);
+        if(!ITEM_READ(Value_Genetic_Distance_Index)) {
+            batchf(Value_Genetic_Distance_Index);
+            batchf(Value_Genetic_Distance_SexTypeMap);
+        }
 
         /* Reorder the loci */
         Tod tod_reorder("ReOrderLoci");
